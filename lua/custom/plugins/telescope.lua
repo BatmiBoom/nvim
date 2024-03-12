afc4b06ed05ec001b8686de162d6cf49a7586175
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-03-12 10:48:05 -0300

    Change telescope layout

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -21,9 +21,22 @@ return { -- Fuzzy Finder (files, lsp, etc)
       end,
       {},
     },
+    {
+      '<leader>sg',
+      function()
+        require('telescope.builtin').live_grep()
+      end,
+      {},
+    },
   },
   config = function()
     require('telescope').setup {
+      defaults = {
+        layout_strategy = 'center',
+        layout_config = {
+          width = 150,
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
@@ -38,7 +51,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
