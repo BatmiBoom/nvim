f237963866716cb68f8ed489fd2267e1b846b454
Nicolas Stirnemann <nicolas@brainhi.com>
2024-05-20 11:14:51 -0300

    Config dbee

--- lua/custom/plugins/dbee.lua
+++ lua/custom/plugins/dbee.lua
@@ -6,7 +6,20 @@ return {
       require('dbee').install()
     end,
     config = function()
-      require('dbee').setup {}
+      require('dbee').setup {
+        sources = {
+          -- require('dbee.sources').MemorySource:new { -- LOAD FROM MEMORY
+          --   {
+          --     name = '...',
+          --     type = '...',
+          --     url = '...',
+          --   },
+          --   -- ...
+          -- },
+          -- require('dbee.sources').EnvSource:new 'DBEE_CONNECTIONS', -- LOAD FROM ENV VARIABLES
+          require('dbee.sources').FileSource:new(vim.fn.stdpath 'cache' .. '/dbee/persistence.json'), -- LOAD FROM JSON FILE
+        },
+      }
 
       ---@diagnostic disable-next-line: param-type-mismatch
       local base = vim.fs.joinpath(vim.fn.stdpath 'state', 'dbee', 'notes')
