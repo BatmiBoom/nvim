01957e955518ecda42863394f983015e8f79b1dd
Nicolas Stirnemann <nicolas@brainhi.com>
2024-05-23 23:24:35 -0300

    Added mini.pairs

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
   'echasnovski/mini.ai',
   dependencies = {
     'echasnovski/mini.surround',
+    'echasnovski/mini.pairs',
   },
   event = { 'InsertEnter' },
   config = function()
@@ -16,6 +17,21 @@ return {
     -- saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- sd'   - [S]urround [D]elete [']quotes
     -- sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        add = 'ya', -- Add surrounding in Normal and Visual modes
+        delete = 'yd', -- Delete surrounding
+        find = 'yf', -- Find surrounding (to the right)
+        find_left = 'yF', -- Find surrounding (to the left)
+        highlight = 'yh', -- Highlight surrounding
+        replace = 'yr', -- Replace surrounding
+        update_n_lines = 'yn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
+
+    require('mini.pairs').setup()
   end,
 }
