48ad95ec0fb473285d8cdfb9b2bf8454d13b101d
Nicolas Stirnemann <nicolas@brainhi.com>
2024-06-26 12:38:37 -0300

    Fix markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,10 +1,2 @@
 vim.opt_local.wrap = true
 vim.opt_local.spell = true
-
--- refresh codelens on TextChanged and InsertLeave as well
-vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave', 'CursorHold', 'LspAttach' }, {
-  buffer = 0,
-  callback = vim.lsp.codelens.refresh,
-})
-
-vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
