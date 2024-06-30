24aaa10af7a1ae9282c31a048f0f9cbddf9f00ab
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-06-30 20:01:41 -0300

    Add formatter

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,27 +1,47 @@
 return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function()
-      return {
-        timeout_ms = 500,
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'ruff_fix', 'ruff_format' },
-      javascript = { 'prettierd' },
-      typescript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-      css = { 'prettierd' },
-      html = { 'prettierd' },
-      json = { 'prettierd' },
-      svelte = { 'prettierd' },
-      yaml = { 'prettierd' },
-      graphql = { 'prettierd' },
-      markdown = { 'prettierd' },
-    },
-  },
+  cmd = { 'ConformInfo' },
+  config = function()
+    local slow_format_filetypes = {}
+    require('conform').setup {
+      notify_on_error = true,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        css = { 'prettierd' },
+        html = { 'prettierd' },
+        json = { 'prettierd' },
+        svelte = { 'prettierd' },
+        yaml = { 'prettierd' },
+        graphql = { 'prettierd' },
+        markdown = { 'prettierd' },
+      },
+      format_on_save = function(bufnr)
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        local function on_format(err)
+          if err and err:match 'timeout$' then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+          end
+        end
+
+        return { timeout_ms = 200, lsp_format = 'fallback' }, on_format
+      end,
+      format_after_save = function(bufnr)
+        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        return { lsp_format = 'fallback' }
+      end,
+    }
+  end,
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
