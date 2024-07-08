3d66c7aa221ee57a30f88e2f0cc25e6bd6db3069
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-07-08 19:41:44 -0300

    config telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -9,24 +9,26 @@ return {
   keys = {
     {
       '<leader><leader>',
-      function()
-        require('telescope.builtin').find_files()
-      end,
-      {},
+      require('telescope.builtin').find_files,
     },
     {
       '<leader>fw',
-      function()
-        require('telescope.builtin').live_grep()
-      end,
-      {},
+      require('telescope.builtin').live_grep,
     },
   },
   config = function()
     require('telescope').setup {
       defaults = {
+        layout_strategy = 'flex',
         layout_config = {
-          vertical = { width = 0.5 },
+          horizontal = {
+            width = 0.9,
+            height = 0.4,
+          },
+          vertical = {
+            width = 0.9,
+            height = 0.9,
+          },
         },
       },
     }
@@ -35,10 +37,7 @@ return {
 
     local telescope = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>fg', telescope.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>fd', telescope.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>fr', telescope.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>f.', telescope.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = '[ ] Find existing buffers' })
   end,
 }
