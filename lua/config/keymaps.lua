4b5587284241bb59b00ab1bf510b5dd4e59571bb
Nicolas Stirnemann <nstirnemann59@gmail.com>
2025-07-15 02:59:57 -0300

    Debug

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,7 @@ vim.keymap.set('n', '<ESC>', '<CMD>noh<CR>', { desc = 'Remove Highlighting' })
 
 vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open TreeFile' })
 
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 -- Copy to clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
