9feb86567523f0111284312373eb9e024bf02f31
Nicolas Stirnemann <nstirnemann59@gmail.com>
2025-07-15 02:32:17 -0300

    New ColorScheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,17 +1,39 @@
 return {
-  'rebelot/kanagawa.nvim',
-  priority = 1000,
-  lazy = false,
-  build = function()
-    vim.cmd 'KanagawaCompile'
-  end,
-  config = function()
-    require('kanagawa').setup {
-      compile = true,
-      transparent = false,
-      theme = 'dragon',
-    }
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    lazy = true,
+    build = function()
+      vim.cmd 'KanagawaCompile'
+    end,
+    config = function()
+      require('kanagawa').setup {
+        compile = true,
+        transparent = false,
+        theme = 'dragon',
+      }
 
-    vim.cmd 'colorscheme kanagawa'
-  end,
+      vim.cmd 'colorscheme kanagawa'
+    end,
+  },
+  {
+    'github-main-user/lytmode.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('lytmode').setup {
+        transparent = false,
+        italic_comments = true,
+        italic_inlayhints = true,
+        underline_links = true,
+        disable_nvimtree_bg = true,
+        terminal_colors = true,
+        color_overrides = {
+          lytLineNumber = '#FFFFFF',
+        },
+      }
+
+      vim.cmd 'colorscheme lytmode'
+    end,
+  },
 }
