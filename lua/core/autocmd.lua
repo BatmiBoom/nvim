909776ee48f6f2857ddeaa5f26bc18ea455586a9
Nicolas Stirnemann <nicolas@brainhi.com>
2025-09-06 11:32:23 -0300

    Deleted snippets

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -44,7 +44,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd("FileType", {
     group = augroup,
-    pattern = { "help", "minifiles" },
+    pattern = { "help", "minifiles", "nvim-pack" },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
         vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,50 +1,46 @@
-vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
-vim.pack.add({ "https://github.com/saghen/blink.cmp" })
-vim.pack.add({ "https://github.com/L3MON4D3/LuaSnip" })
-vim.pack.add({ "https://github.com/rafamadriz/friendly-snippets" })
-
-require("blink.cmp").setup({
-	keymap = {
-		preset = "default",
-	},
-	completion = {
-		menu = {
-			border = "single",
-			draw = {
-				components = {
-					kind_icon = {
-						text = function(ctx)
-							local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-							return kind_icon
-						end,
-						highlight = function(ctx)
-							local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-							return hl
-						end,
-					},
-					kind = {
-						highlight = function(ctx)
-							local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-							return hl
-						end,
-					},
-				},
-			},
-		},
-		documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
-	},
-	sources = {
-		providers = {
-			lsp = {
-				name = "LSP",
-				module = "blink.cmp.sources.lsp",
-			},
-		},
-		default = { "lsp", "path", "snippets" },
-	},
-	snippets = { preset = "luasnip" },
-	fuzzy = { implementation = "lua", sorts = { "exact", "score", "sort_text" } },
-	signature = { enabled = true },
+vim.pack.add({
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/saghen/blink.cmp",     version = "v1.6.0" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
-require("luasnip.loaders.from_vscode").lazy_load()
+require("blink.cmp").setup({
+    keymap = {
+        preset = "default",
+    },
+    completion = {
+        menu = {
+            draw = {
+                columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+                components = {
+                    kind_icon = {
+                        text = function(ctx)
+                            local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                            return kind_icon
+                        end,
+                        highlight = function(ctx)
+                            local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                            return hl
+                        end,
+                    },
+                    kind = {
+                        highlight = function(ctx)
+                            local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                            return hl
+                        end,
+                    },
+                },
+            },
+        },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
+    },
+    sources = {
+        default = { "lsp", "path", "buffer" },
+    },
+    appearance = {
+        highlight_ns = vim.api.nvim_create_namespace('blink_cmp'),
+        nerd_font_variant = 'normal',
+    },
+    fuzzy = { implementation = "prefer_rust_with_warning", sorts = { "exact", "score", "sort_text" } },
+    signature = { enabled = true },
+})
