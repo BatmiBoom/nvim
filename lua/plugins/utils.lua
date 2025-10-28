833901dc189d2bd26c8c8b10e375cee3d214c46f
Nicolas Stirnemann <nicolas@brainhi.com>
2025-10-28 10:22:26 -0300

    Setup mini icons

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,5 +1,7 @@
-vim.pack.add({
-    { src = "https://github.com/nvim-mini/mini.icons" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/sindrets/diffview.nvim" }
-})
+vim.pack.add {
+  { src = 'https://github.com/nvim-mini/mini.icons' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/sindrets/diffview.nvim' },
+}
+
+require('mini.icons').setup()
